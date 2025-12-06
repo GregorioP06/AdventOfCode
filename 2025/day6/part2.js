3ef5001be336d66931e93112404b8d0a71a1273c
GregorioP06 <g.pescucci2@studenti.unipi.it>
2025-12-06 15:52:29 +0100

    migliorato day6

--- 2025/day6/part2.js
+++ 2025/day6/part2.js
@@ -10,35 +10,36 @@ input = input.map((x) => x.replace("\r", "")).filter((x) => x);
 
 function main(input_data) {
     let total = 0;
-    // scorri l'ultima riga (dove ci sono gli operatori)
-    // gli operatori delineano l'inizio della colonna
     const last_row = input_data[input_data.length - 1];
-    let last_seen_idx = 0;
-    let column = [];
+    let col_start_idx = 0; // salva l'indice a cui è iniziata la colonna
+    // Scorri l'ultima riga (dove ci sono gli operatori).
+    // Gli operatori indicano l'inizio delle colonne
     for (let j = 1; j < last_row.length; j++) {
+        const column = [];
+        // se si trova l'inizio di una nuova colonna
+        // o se siamo alla fine (ultima colonna)
         if (
             last_row[j] === "+" ||
             last_row[j] === "*" ||
             j === last_row.length - 1
         ) {
-            if (j === last_row.length - 1) {
-                j = Math.max(...input_data.map((line) => line.length));
-            }
+            // se è siamo alla fine incrementa j per non perdere l'ultima colonna
+            if (j === last_row.length - 1) j += 2;
             for (let i = 0; i < input_data.length; i++) {
-                let row = input_data[i].slice(last_seen_idx, j);
-                if (row.at(-1) === " ") column.push(row.slice(0, -1));
-                else column.push(row);
+                // da ogni riga prendi il pezzo da mettere nella colonna
+                // j - 1 per togliere lo spazio inutile
+                column.push(input_data[i].slice(col_start_idx, j - 1));
             }
-            last_seen_idx = j;
+            col_start_idx = j;
             total += solve_column(column);
-            column = [];
         }
     }
     return total;
 }
 
 function solve_column(column) {
-    let numbers = [];
+    const numbers = [];
+    // scorri l'array verticalmente salvando i numeri così formati in numbers
     for (let j = 0; j < column[0].length; j++) {
         let number = "";
         for (let i = 0; i < column.length - 1; i++) {
@@ -46,6 +47,7 @@ function solve_column(column) {
         }
         numbers.push(parseInt(number));
     }
+    // applica l'operazione giusta a tutti i numeri
     if (column.at(-1)[0] === "+")
         return numbers.reduce((acc, val) => acc + val);
     else if (column.at(-1)[0] === "*")
